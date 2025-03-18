302ae5ed36e76ab65815f20d95c5346de32a9320
Grazen0 <josedanielgrayson@proton.me>
2025-03-18 18:41:22 -0500

    feat(lib): refactor icons module and add icons for more languages

--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -2,44 +2,62 @@ import {
 	siAssemblyscript,
 	siC,
 	siCplusplus,
+	siCss3,
+	siGo,
 	siHaskell,
+	siHtml5,
 	siJavascript,
+	siJulia,
+	siKotlin,
+	siLua,
 	siNixos,
+	siOpenjdk,
 	siPerl,
+	siPhp,
 	siPython,
+	siR,
+	siRuby,
 	siRust,
+	siScala,
 	siSharp,
+	siSvelte,
 	siTypescript,
+	siVuedotjs,
+	siZig,
 	type SimpleIcon,
 } from 'simple-icons';
 
+const icons: Record<string, SimpleIcon> = {
+	c: siC,
+	'c++': siCplusplus,
+	'c#': siSharp,
+	html: siHtml5,
+	css: siCss3,
+	javascript: siJavascript,
+	typescript: siTypescript,
+	python: siPython,
+	php: siPhp,
+	rust: siRust,
+	nix: siNixos,
+	ruby: siRuby,
+	julia: siJulia,
+	scala: siScala,
+	haskell: siHaskell,
+	java: siOpenjdk,
+	svelte: siSvelte,
+	vue: siVuedotjs,
+	kotlin: siKotlin,
+	assembly: siAssemblyscript,
+	lua: siLua,
+	go: siGo,
+	zig: siZig,
+	perl: siPerl,
+	r: siR,
+};
+
 export const iconByProgrammingLanguage = (lang: string): SimpleIcon | null => {
-	switch (lang.toLowerCase()) {
-		case 'c':
-			return siC;
-		case 'c++':
-			return siCplusplus;
-		case 'c#':
-			return siSharp;
-		case 'javascript':
-			return siJavascript;
-		case 'typescript':
-			return siTypescript;
-		case 'python':
-			return siPython;
-		case 'rust':
-			return siRust;
-		case 'nix':
-			return siNixos;
-		case 'haskell':
-			return siHaskell;
-		case 'assembly':
-			return siAssemblyscript;
-		case 'perl':
-			return siPerl;
-		default:
-			return null;
-	}
+	lang = lang.toLowerCase();
+	return icons[lang] ?? null;
 };
 
 // TODO: color by programming language

2f824bcaa3722a91a231d3f011e7d1e5077e9a0e
Grazen0 <josedanielgrayson@proton.me>
2025-03-18 19:36:30 -0500

    build: enable enhanced images

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { svelteTesting } from '@testing-library/svelte/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [tailwindcss(), sveltekit(), enhancedImages()],
 
 	test: {
 		workspace: [

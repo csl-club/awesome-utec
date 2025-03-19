aade3fb57acd37156fe6754d96fa31c83ed3af2d
Grazen0 <josedanielgrayson@proton.me>
2025-03-19 17:52:57 -0500

    feat: add language colors

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -4,36 +4,41 @@
 	import type { HTMLLiAttributes } from 'svelte/elements';
 	import { repoStringToUrl } from '$lib/repo';
 	import ExternalLink from '$lib/components/ExternalLink.svelte';
-	import { iconByProgrammingLanguage } from '$lib/icons';
+	import { iconByProgrammingLanguage, colorByProgrammingLanguage } from '$lib/icons';
 	import SimpleIconComponent from './SimpleIconComponent.svelte';
 	import { Link } from '@lucide/svelte';
+	import { readableColor } from 'polished';
 
 	export interface Props extends HTMLLiAttributes {
 		project: Project;
 	}
 
 	const { project, class: className, ...props }: Props = $props();
+	const { lang } = project;
 
 	const projectUrl = $derived(repoStringToUrl(project.repo));
-	const langIcon = $derived(project.lang ? iconByProgrammingLanguage(project.lang) : null);
+	const langIcon = $derived(lang ? iconByProgrammingLanguage(lang) : null);
+	const langBackground = $derived(
+		(lang && colorByProgrammingLanguage(lang)) || 'var(--color-background)',
+	);
+	const langForeground = $derived(readableColor(langBackground));
 </script>
 
 <li {...props} class={classNames(className, 'bg-background-alt flex flex-col px-3 py-2')}>
-	<div class="flex justify-between">
+	<div class="flex items-start justify-between">
 		<h2 class="text-xl font-semibold">
 			<ExternalLink href={projectUrl}>
 				{project.name}
 				<Link class="text-foreground-muted inline w-3" />
 			</ExternalLink>
 		</h2>
-		<span>
+		<span
+			style="background-color: {langBackground}; color: {langForeground}"
+			class="h-auto px-2 py-1 text-xs text-nowrap"
+		>
 			{#if langIcon !== null}
-				<SimpleIconComponent
-					icon={langIcon}
-					fill="var(--color-foreground)"
-					class="inline h-4 w-4"
-				/>
-			{/if}<span class="ml-2 text-sm">{project.lang}</span>
+				<SimpleIconComponent icon={langIcon} fill={langForeground} class="inline h-3 w-3" />
+			{/if}<span class="ml-2">{project.lang}</span>
 		</span>
 	</div>
 	<p>{project.authors.map((author) => author.name).join(', ')}</p>
--- src/lib/icons.ts
+++ src/lib/icons.ts
@@ -26,8 +26,34 @@ import {
 	siZig,
 	type SimpleIcon,
 } from 'simple-icons';
+import lingC from 'linguist-languages/data/C';
+import lingCplusplus from 'linguist-languages/data/C++';
+import lingCSharp from 'linguist-languages/data/C#';
+import lingHtml5 from 'linguist-languages/data/HTML';
+import lingCss3 from 'linguist-languages/data/CSS';
+import lingJavaScript from 'linguist-languages/data/JavaScript';
+import lingTypeScript from 'linguist-languages/data/TypeScript';
+import lingPython from 'linguist-languages/data/Python';
+import lingPhp from 'linguist-languages/data/PHP';
+import lingRust from 'linguist-languages/data/Rust';
+import lingNixos from 'linguist-languages/data/Nix';
+import lingRuby from 'linguist-languages/data/Ruby';
+import lingJulia from 'linguist-languages/data/Julia';
+import lingScala from 'linguist-languages/data/Scala';
+import lingHaskell from 'linguist-languages/data/Haskell';
+import lingOpenjdk from 'linguist-languages/data/Java';
+import lingSvelte from 'linguist-languages/data/Svelte';
+import lingVuedotjs from 'linguist-languages/data/Vue';
+import lingKotlin from 'linguist-languages/data/Kotlin';
+import lingAssembly from 'linguist-languages/data/Assembly';
+import lingLua from 'linguist-languages/data/Lua';
+import lingGo from 'linguist-languages/data/Go';
+import lingZig from 'linguist-languages/data/Zig';
+import lingPerl from 'linguist-languages/data/Perl';
+import lingR from 'linguist-languages/data/R';
+import type { Language } from 'linguist-languages';
 
-const icons: Record<string, SimpleIcon> = {
+const icons: Record<string, SimpleIcon | undefined> = {
 	c: siC,
 	'c++': siCplusplus,
 	'c#': siSharp,
@@ -55,9 +81,40 @@ const icons: Record<string, SimpleIcon> = {
 	r: siR,
 };
 
+const langInfo: Record<string, Language | undefined> = {
+	c: lingC,
+	'c++': lingCplusplus,
+	'c#': lingCSharp,
+	html: lingHtml5,
+	css: lingCss3,
+	javascript: lingJavaScript,
+	typescript: lingTypeScript,
+	python: lingPython,
+	php: lingPhp,
+	rust: lingRust,
+	nix: lingNixos,
+	ruby: lingRuby,
+	julia: lingJulia,
+	scala: lingScala,
+	haskell: lingHaskell,
+	java: lingOpenjdk,
+	svelte: lingSvelte,
+	vue: lingVuedotjs,
+	kotlin: lingKotlin,
+	assembly: lingAssembly,
+	lua: lingLua,
+	go: lingGo,
+	zig: lingZig,
+	perl: lingPerl,
+	r: lingR,
+};
+
 export const iconByProgrammingLanguage = (lang: string): SimpleIcon | null => {
 	lang = lang.toLowerCase();
 	return icons[lang] ?? null;
 };
 
-// TODO: color by programming language
+export const colorByProgrammingLanguage = (lang: string): string | null => {
+	lang = lang.toLowerCase();
+	return langInfo[lang]?.color ?? null;
+};

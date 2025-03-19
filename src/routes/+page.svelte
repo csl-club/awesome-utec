573f1194b5591eaae3edb3d71d248bc04b80ac1d
Grazen0 <josedanielgrayson@proton.me>
2025-03-19 17:56:57 -0500

    feat: add awesome badge under website title

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,13 +16,19 @@
 </svelte:head>
 
 <main class="font-main mx-auto max-w-4xl px-6 py-4">
-	<h1 class="mb-6 text-center text-4xl font-bold">Awesome UTEC</h1>
+	<h1 class="mb-4 text-center text-4xl font-bold">Awesome UTEC</h1>
+	<div class="text-center">
+		<a href="https://awesome.re" aria-label="Awesome">
+			<enhanced:img src="$lib/assets/img/awesome.svg" alt="" class="inline" />
+		</a>
+	</div>
+
 	<p class="my-10 text-center text-lg">
 		Un compendio de proyectos de computación de la Universidad de Ingeniería y Tecnología.
 	</p>
 
 	<div class="space-x-4 text-center">
-		<SearchInput placeholder="Buscar..." bind:value={searchQuery} />
+		<SearchInput placeholder="Buscar..." bind:value={searchQuery} class="text-sm" />
 	</div>
 
 	<ul class="my-8 grid grid-cols-1 gap-4 sm:grid-cols-2">

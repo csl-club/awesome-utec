878a7a473d1fa3204b9a66e57ce5ba5600c1225d
Grazen0 <josedanielgrayson@proton.me>
2025-03-24 15:09:30 -0500

    feat: add sort direction button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,16 +5,27 @@
 	import SearchInput from '$lib/components/SearchInput.svelte';
 	import globalState from '$lib/svelte/global-state.svelte';
 	import { projectSorters, SORT_TYPES, type SortType } from '$lib/sorting';
+	import { ArrowDown, ArrowUp } from '@lucide/svelte';
 
 	const { data }: PageProps = $props();
 
 	let sortType = $state<SortType>('default');
+	let reverseSort = $state(false);
 
 	const sorter = $derived(projectSorters[sortType]);
 
-	const filteredProjects = $derived(
-		data.projects.filter(doesProjectMatchQuery(globalState.searchQuery)).sort(sorter.sort),
-	);
+	const filteredProjects = $derived.by(() => {
+		const result = data.projects
+			.filter(doesProjectMatchQuery(globalState.searchQuery))
+			.sort(sorter.sort);
+
+		if (reverseSort) {
+			result.reverse();
+		}
+		return result;
+	});
+
+	const SortDirectionIcon = $derived(reverseSort ? ArrowUp : ArrowDown);
 </script>
 
 <svelte:head>
@@ -34,8 +45,10 @@
 	</p>
 
 	<div class="space-x-4 text-center text-sm">
+		<label for="sortby" class="text-foreground-muted mr-0">Buscar:</label>
 		<SearchInput
-			placeholder="Buscar..."
+			name="sortby"
+			id="sortby"
 			bind:value={globalState.searchQuery}
 			bind:input={globalState.searchInput}
 			class="w-64 "
@@ -46,12 +59,15 @@
 			name="sortby"
 			id="sortby"
 			bind:value={sortType}
-			class="border-foreground border px-2 py-1"
+			class="border-foreground mr-1 border px-2 py-1"
 		>
 			{#each SORT_TYPES as sortType, index (index)}
 				<option value={sortType}>{projectSorters[sortType].displayName}</option>
 			{/each}
 		</select>
+		<button onclick={() => (reverseSort = !reverseSort)}>
+			<SortDirectionIcon class="inline w-5 cursor-pointer" />
+		</button>
 	</div>
 
 	<ul class="my-8 grid grid-cols-1 gap-4 sm:grid-cols-2">
